74175cca28172c6da8846de46e58d602f4554456
Madeline Whitaker <mrwhitaker2@crimson.ua.edu>
2017-04-11 19:00:31 -0500

    search start. no display

--- ThirdDVDStore/src/java/dvdrental/SearchController.java
+++ ThirdDVDStore/src/java/dvdrental/SearchController.java
@@ -7,6 +7,8 @@ package dvdrental;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,27 +25,41 @@ public class SearchController extends HttpServlet {
 
     private static String RESULTS = "/SearchResults.jsp";
     private static String BROWSE = "/CustomerBrowse.jsp";
+    List<Film> films = new ArrayList<Film>();
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
+
         String criteria = request.getParameter("criteria");
         String field = request.getParameter("field");
-        
-        if(criteria.equalsIgnoreCase("Genre"))
-        {
+
+        if (criteria.equalsIgnoreCase("Genre")) {
+            
             FilmDAO.searchGenre(field);
+            request.setAttribute("films", FilmDAO.searchGenre(field));
             RequestDispatcher rs = request.getRequestDispatcher(RESULTS);
             rs.forward(request, response);
+           
+                // RequestDispatcher rs = request.getRequestDispatcher(BROWSE);
+                 //rs.forward(request, response);
+        
+
+        } else if (criteria.equalsIgnoreCase("Actor")) {
+
+            FilmDAO.searchActor(field);
+            RequestDispatcher rs = request.getRequestDispatcher(RESULTS);
+            rs.forward(request, response);
+        } else if (criteria.equalsIgnoreCase("Store")) {
+            //FilmDAO.searchStore(field);
+            RequestDispatcher rs = request.getRequestDispatcher(RESULTS);
+            rs.forward(request, response);
+        } else {
+            out.println("Your search did not provide any results.");
+            RequestDispatcher rs = request.getRequestDispatcher(BROWSE);
+            rs.include(request, response);
         }
-        else
-        {
-           out.println("Your search did not provide any results.");
-           RequestDispatcher rs = request.getRequestDispatcher(BROWSE);
-           rs.include(request, response);
-        }
-    }  
-   
+    }
+
 }
